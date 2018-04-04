623d890b33dc4f4a7890e4b542532db7a1b5fda6
florian <zobeleflorian@gmail.com>
2018-04-05 00:18:34 +0200

    fucking project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
+#define GLEW_STATIC
 #include <iostream>
+#include <GL/glew.h>
+#include <glimac/SDLWindowManager.hpp>
 
 int main(int argc, char** argv) {
 
-  std::cout << "ok" << std::endl;
-  
+  /* -------------------------- */
+  /* ----- INITIALIZATION ----- */
+  /* -------------------------- */
+
+  glimac::SDLWindowManager windowManager(800, 800, "SolarSystem");
+
+  GLenum glewInitError = glewInit();
+  if(GLEW_OK != glewInitError) {
+    std::cerr << glewGetErrorString(glewInitError) << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
+  std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+
+  /* --------------------- */
+  /* ----- MAIN CODE ----- */
+  /* --------------------- */
+
   return EXIT_SUCCESS;
 }
